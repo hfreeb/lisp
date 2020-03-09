4bc65a2a80e7823c2b1943a9eb0bea36ca963f58
Harry Freeborough <harry@hfreeb.com>
2020-03-09 00:06:28 +0000

    Split function call implementation in the interpreter

--- src/main/kotlin/com/desetude/lisp/interpreter/Interpreter.kt
+++ src/main/kotlin/com/desetude/lisp/interpreter/Interpreter.kt
@@ -9,29 +9,30 @@ object Interpreter {
             is Expression.Symbol -> env.lookup(expression.name) ?: throw IllegalStateException("Variable with label ${expression.name} not found")
             is Expression.LinkedList -> {
                 when(val func = eval(env, expression.value)) {
-                    is Expression.BuiltInFunction -> {
-                        func.handler(env, expression.next)
-                    }
-                    is Expression.Function -> {
-                        val newEnv = Environment(env)
-                        var headParams: Expression.LinkedList? = func.params
-                        var headArgs = expression.next
-                        while (headParams != null && headArgs != null) {
-                            val param = headParams.value
-                            val arg = headArgs.value
-                            require(param is Expression.Symbol)
-
-                            newEnv.set(param.name, eval(env, arg))
-
-                            headParams = headParams.next
-                            headArgs = headArgs.next
-                        }
-                        require(headParams == null && headArgs == null) { "Mismatch between number of parameters and number of arguments" }
-                        return eval(newEnv, func.body)
-                    }
+                    is Expression.BuiltInFunction -> func.handler(env, expression.next)
+                    is Expression.Function -> callFunction(env, func, expression.next)
                     else -> throw IllegalStateException("First value in any s-expression must be a function")
                 }
             }
         }
+    }
+
+    private fun callFunction(env: Environment, func: Expression.Function, args: Expression.LinkedList?): Expression {
+        val newEnv = Environment(env)
+        var headParams: Expression.LinkedList? = func.params
+        var headArgs = args
+        while (headParams != null && headArgs != null) {
+            val param = headParams.value
+            val arg = headArgs.value
+            require(param is Expression.Symbol)
+
+            newEnv.set(param.name, eval(env, arg))
+
+            headParams = headParams.next
+            headArgs = headArgs.next
+        }
+
+        require(headParams == null && headArgs == null) { "Mismatch between number of parameters and number of arguments" }
+        return eval(newEnv, func.body)
     }
 }
